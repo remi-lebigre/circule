cc70a75b56a7cdea17f03d1fa7c88ac673749abd
remi <remi@148.fr>
2020-11-15 23:18:17 +0100

    Update webpack config for production build

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '/',
+    publicPath: './',
   },
   module: {
     rules: [
@@ -18,7 +19,7 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          {loader: 'style-loader'},
+          MiniCssExtractPlugin.loader,
           {loader: 'css-loader', options: {sourceMap: true}},
           {loader: 'postcss-loader', options: {sourceMap: true}},
           {loader: 'sass-loader', options: {sourceMap: true}},
@@ -46,14 +47,10 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.pug'
     }),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9002,
-    open: true,
-  }
 };
