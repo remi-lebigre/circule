0f7f93dd0fba900ba07603474ba7814a30b7d27c
remi <remi@148.fr>
2020-11-16 23:38:52 +0100

    Place footer elements

--- src/js/i.js
+++ src/js/i.js
@@ -1,5 +1,5 @@
 class I {
-  tags = document.querySelectorAll('h1, h2, h3, h4, h5, h6, .text-link, .text-callout')
+  tags = document.querySelectorAll('h1, h2, h3, h4, h5, h6, .text-h1, .text-h4, .text-link, .text-callout')
 
   constructor () {
     this.tags.forEach(tag => this.recursive(tag, 3))
