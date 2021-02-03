f70955143f65de872f31fbdabc95ea61ccd2751d
remi <remi@148.fr>
2021-02-03 12:25:17 +0100

    Test mailer API with other syntax

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,1 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  })
-}
+export default (request, response) => response.send('hello world');
