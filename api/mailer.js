85faf7b922a3472caff13a88f05b8a25c0806fa8
remi <remi@148.fr>
2021-02-06 18:17:10 +0100

    Add mailer api

--- api/mailer.js
+++ api/mailer.js
@@ -5,9 +5,8 @@ const sendEmail = response => {
   const msg = {
     to: 'remi.lebigre@gmail.com',
     from: 'remi.lebigre@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    subject: 'Circule booking',
+    html: '<strong>Hello :)</strong>',
   }
   sgMail
     .send(msg)
@@ -15,12 +14,15 @@ const sendEmail = response => {
       response.send('Email sent')
     })
     .catch((error) => {
-      response.send(error)
+      console.log(error)
+      response.send('Email not sent')
     })
 }
 
 export default (req, response) => {
-  if (req.param.send) {
+  if (req.query.send === process.env.MAILER_TOKEN) {
     sendEmail(response)
+  } else {
+    response.send('ohla')
   }
-};
+}
